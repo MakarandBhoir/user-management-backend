d33609cf3306e049fdfff4bf513989487ed9ff2a
makarand.bhoir <makarand.bhoir@live.com>
2026-05-05 18:16:25 +0530

    Add CrossOrigin annotation to DemoController and UserController for CORS support

--- src/main/java/com/tcs/usermanagement/controller/DemoController.java
+++ src/main/java/com/tcs/usermanagement/controller/DemoController.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +18,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/demo")
+@CrossOrigin(origins = "*")
 public class DemoController {
 
     private static final Logger log = LoggerFactory.getLogger(DemoController.class);
--- src/main/java/com/tcs/usermanagement/controller/UserController.java
+++ src/main/java/com/tcs/usermanagement/controller/UserController.java
@@ -5,6 +5,7 @@ import com.tcs.usermanagement.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
